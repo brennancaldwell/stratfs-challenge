927228a8a2734d4ba644b6e43d840af95ee3c9a9
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-07 13:31:18 -0500

    Implement GET request using Axios

--- debt-spreadsheet/src/App.js
+++ debt-spreadsheet/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ allData, setAllData ] = useState([]);
+  const [ selected, setSelected ] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json')
+          .then((data) => setAllData(data.data));
+  }, []);
+
+  console.log(allData);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi <code>src/App.js</code> and save to reload.
         </p>
