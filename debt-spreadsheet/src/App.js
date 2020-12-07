0cb5efbbc8c12b83b0a02e99a52d71f7b657d606
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-07 15:48:39 -0500

    Implement addDebt functionality

--- debt-spreadsheet/src/App.js
+++ debt-spreadsheet/src/App.js
@@ -77,6 +77,7 @@ function App() {
         <AddDebt
           addDebt={addDebt}
           toggleModal={toggleModal}
+          allData={allData}
         />
       </Modal>
 
--- debt-spreadsheet/src/components/AddDebt.js
+++ debt-spreadsheet/src/components/AddDebt.js
@@ -11,15 +11,40 @@ export default class AddDebt extends React.Component {
       balance: ''
     }
 
-    const { addDebt, toggleModal } = this.props;
-
     this.onChange = this.onChange.bind(this);
+    this.submitDebt = this.submitDebt.bind(this);
   }
 
   onChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
+  }
+
+  submitDebt() {
+    const { creditorName, firstName, lastName, minPaymentPercentage, balance } = this.state;
+
+    const { addDebt, toggleModal, allData } = this.props;
+
+    let id = allData.reduce((a, b) => {
+      if (a < b.id) {
+        return b.id
+      } else {
+        return a;
+      }
+    }, 0) + 1;
+
+    if (isNaN(Number(minPaymentPercentage)) && isNaN(Number(balance))) {
+      throw new Error('Please enter a number for Min Pay % and Balance!')
+    } else if (isNaN(Number(minPaymentPercentage))) {
+      throw new Error ('Please enter a number for Min Pay %!')
+    } else if (isNaN(Number(balance))) {
+      throw new Error ('Please enter a number for Balance!')
+    }
+
+    addDebt({ id, creditorName, firstName, lastName, minPaymentPercentage, balance});
+
+    toggleModal();
   }
 
 
@@ -65,7 +90,12 @@ export default class AddDebt extends React.Component {
           onChange={this.onChange}
           value={balance}
           /> <br />
-          <button id="addModal">Submit Debt</button>
+          <button
+            className="addModal"
+            onClick={this.submitDebt}
+          >
+            Submit Debt
+          </button>
       </div>
     );
   }
