225a3ef6d5907d74d4257a10bf6d096e17fbe957
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-12-07 14:43:12 -0500

    Implement modal with react-modal for add debt functionality

--- debt-spreadsheet/src/App.js
+++ debt-spreadsheet/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from './components/Table';
+import Modal from 'react-modal';
 import './App.css';
 
 function App() {
   const [ allData, setAllData ] = useState([]);
   const [ selected, setSelected ] = useState([]);
+  const [ modalOpen, setModalOpen ] = useState(false);
 
   function selectOne(e) {
     let newSelected = selected.slice();
@@ -31,12 +33,18 @@ function App() {
     setSelected([]);
   }
 
+  function toggleModal() {
+    setModalOpen(!modalOpen);
+  }
+
   useEffect(() => {
     axios.get('https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json')
           .then((data) => setAllData(data.data));
   }, []);
 
   console.log(selected);
+
+  if (allData.length === 0) return (<h1>Loading...</h1>)
 
   return (
     <div className="App">
@@ -46,9 +54,19 @@ function App() {
         selectOne={selectOne}
         selectAll={selectAll}
       />
+      <button className="add" onClick={toggleModal}>
+        Add Debt
+      </button>
       <button onClick={removeDebt}>
         Remove Debt
       </button>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={toggleModal}
+      >
+        hello world
+      </Modal>
+
     </div>
   );
 }
